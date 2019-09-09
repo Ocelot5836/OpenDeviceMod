b009a8fe42f2250089a59d961fc919b4e3a8f0df
Ocelot5836 <FinntheRaider@gmail.com>
2019-09-08 18:08:08 -0700

    added network message framework

---
+++ src/main/java/com/ocelot/opendevices/init/DeviceMessages.java
@@ -0,0 +1,32 @@
+package com.ocelot.opendevices.init;
+
+import com.ocelot.opendevices.OpenDevices;
+import com.ocelot.opendevices.network.MessageCloseLaptop;
+import com.ocelot.opendevices.network.MessageCloseLaptopHandler;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class DeviceMessages
+{
+    public static final String VERSION = "1.0";
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(new ResourceLocation(OpenDevices.MOD_ID, "instance"), () -> VERSION, VERSION::equals, VERSION::equals);
+
+    private static int index;
+
+    public static void init()
+    {
+        registerMessage(MessageCloseLaptop.class, MessageCloseLaptop::encode, MessageCloseLaptop::decode, MessageCloseLaptopHandler::handle);
+    }
+
+    private static <MSG> void registerMessage(Class<MSG> messageType, BiConsumer<MSG, PacketBuffer> encoder, Function<PacketBuffer, MSG> decoder, BiConsumer<MSG, Supplier<NetworkEvent.Context>> messageConsumer)
+    {
+        INSTANCE.registerMessage(index++, messageType, encoder, decoder, messageConsumer);
+    }
+}
---
+++ src/main/java/com/ocelot/opendevices/network/MessageCloseLaptopHandler.java
@@ -0,0 +1,33 @@
+package com.ocelot.opendevices.network;
+
+import java.util.function.Supplier;
+
+import com.ocelot.opendevices.tileentity.LaptopTileEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+public class MessageCloseLaptopHandler
+{
+    public static void handle(MessageCloseLaptop msg, Supplier<NetworkEvent.Context> ctx)
+    {
+        ctx.get().enqueueWork(() ->
+        {
+            ServerPlayerEntity player = ctx.get().getSender();
+            if (player != null)
+            {
+                World world = player.world;
+                BlockPos pos = msg.pos;
+                if (world.getTileEntity(pos) instanceof LaptopTileEntity)
+                {
+                    LaptopTileEntity laptop = (LaptopTileEntity) world.getTileEntity(pos);
+                    assert laptop != null;
+                    laptop.stopView(player);
+                }
+            }
+        });
+        ctx.get().setPacketHandled(true);
+    }
+}
