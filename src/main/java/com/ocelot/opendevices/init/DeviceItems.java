8f0f3cc6763f0000457bdf42e2369ee0c6a6cc23
Ocelot5836 <FinntheRaider@gmail.com>
2020-02-24 20:14:12 -0800

    added some material items

--- src/main/java/com/ocelot/opendevices/init/DeviceItems.java
+++ src/main/java/com/ocelot/opendevices/init/DeviceItems.java
@@ -1,16 +1,28 @@
 package com.ocelot.opendevices.init;
 
+import com.ocelot.opendevices.item.ModItem;
 import net.minecraft.item.Item;
 import org.apache.commons.lang3.Validate;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class DeviceItems
 {
     private static final Set<Item> ITEMS = new HashSet<>();
+    private static final Map<DeviceMaterials, Item> MATERIALS = new HashMap<>();
 
-    // TODO add items
+    static
+    {
+        for (DeviceMaterials material : DeviceMaterials.values())
+        {
+            Item materialItem = new ModItem(material.getRegistryName(), new Item.Properties().group(material.getGroup()));
+            MATERIALS.put(material, materialItem);
+            register(materialItem);
+        }
+    }
 
     public static Item register(Item item)
     {
@@ -22,5 +34,10 @@ public class DeviceItems
     public static Item[] getItems()
     {
         return ITEMS.toArray(new Item[0]);
+    }
+
+    public static Item getMaterialItem(DeviceMaterials material)
+    {
+        return MATERIALS.get(material);
     }
 }
---
+++ src/main/java/com/ocelot/opendevices/init/DeviceMaterials.java
@@ -0,0 +1,65 @@
+package com.ocelot.opendevices.init;
+
+import com.ocelot.opendevices.OpenDevices;
+import net.minecraft.item.ItemGroup;
+
+public enum DeviceMaterials
+{
+    UNREFINED_PLASTIC("unrefined_plastic"),
+    PLASTIC("plastic"),
+    PLASTIC_FRAME("plastic_frame"),
+    BATTERY("battery"),
+    SCREEN("screen"),
+
+    WHEEL("wheel"),
+    SMALL_ELECTRIC_MOTOR("small_electric_motor"),
+    CARRIAGE("carriage"),
+
+    FLASH_CHIP("flash_chip"),
+    CONTROLLER_CHIP("controller_chip"),
+
+    // TODO add colors to drives
+    HARD_DRIVE("hard_drive"),
+    SOLID_STATE_DRIVE("solid_state_drive"),
+    FLOPPY_DRIVE("floppy_drive"),
+    FLASH_DRIVE("flash_drive"),
+
+    FIBERGLASS_PANEL("fiberglass_panel"),
+    GREEN_CIRCUIT_BOARD("green_circuit_board"),
+    BLUE_CIRCUIT_BOARD("blue_circuit_board"),
+    RED_CIRCUIT_BOARD("red_circuit_board"),
+
+    ;
+
+    /*
+        COMPONENT_MOTHERBOARD = new ItemMotherboard();
+        COMPONENT_CPU = new ItemMotherboard.Component("cpu");
+        COMPONENT_RAM = new ItemMotherboard.Component("ram");
+        COMPONENT_GPU = new ItemMotherboard.Component("gpu");
+        COMPONENT_WIFI = new ItemMotherboard.Component("wifi");
+     */
+
+    private String registryName;
+    private ItemGroup group;
+
+    DeviceMaterials(String registryName)
+    {
+        this(registryName, OpenDevices.TAB);
+    }
+
+    DeviceMaterials(String registryName, ItemGroup group)
+    {
+        this.registryName = registryName;
+        this.group = group;
+    }
+
+    public String getRegistryName()
+    {
+        return registryName;
+    }
+
+    public ItemGroup getGroup()
+    {
+        return group;
+    }
+}
