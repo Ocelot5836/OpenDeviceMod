ccfe9a9b4712b0932cac1a747fb53a0abd6f6df5
Ocelot5836 <FinntheRaider@gmail.com>
2020-02-25 15:56:57 -0800

    Update TestProcessInputHandler.java

--- src/main/java/com/ocelot/opendevices/core/laptop/process/TestProcessInputHandler.java
+++ src/main/java/com/ocelot/opendevices/core/laptop/process/TestProcessInputHandler.java
@@ -9,44 +9,5 @@ import java.util.UUID;
 
 public class TestProcessInputHandler implements ProcessInputHandler<Computer, TestProcess>
 {
-    @Override
-    public boolean onKeyPressed(TestProcess process, @Nullable UUID windowId, int keyCode, int scanCode, int mods)
-    {
-        return true;
-    }
 
-    @Override
-    public boolean onKeyReleased(TestProcess process, @Nullable UUID windowId, int keyCode, int scanCode, int mods)
-    {
-        return true;
-    }
-
-    @Override
-    public boolean onMouseDragged(TestProcess process, @Nullable UUID windowId, double mouseX, double mouseY, int mouseButton, double deltaX, double deltaY)
-    {
-        return true;
-    }
-
-    @Override
-    public boolean onMousePressed(TestProcess process, @Nullable UUID windowId, double mouseX, double mouseY, int mouseButton)
-    {
-        return true;
-    }
-
-    @Override
-    public boolean onMouseReleased(TestProcess process, @Nullable UUID windowId, double mouseX, double mouseY, int mouseButton)
-    {
-        return true;
-    }
-
-    @Override
-    public boolean onMouseScrolled(TestProcess process, @Nullable UUID windowId, double mouseX, double mouseY, double amount)
-    {
-        return true;
-    }
-
-    @Override
-    public void onMouseMoved(TestProcess process, @Nullable UUID windowId, double mouseX, double mouseY)
-    {
-    }
 }
