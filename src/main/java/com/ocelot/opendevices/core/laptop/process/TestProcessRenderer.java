29ce52b898addb98727fbc6dec1f725783a6eaa9
Ocelot5836 <FinntheRaider@gmail.com>
2020-02-27 16:23:28 -0800

    Update TestProcessRenderer.java

--- src/main/java/com/ocelot/opendevices/core/laptop/process/TestProcessRenderer.java
+++ src/main/java/com/ocelot/opendevices/core/laptop/process/TestProcessRenderer.java
@@ -4,21 +4,20 @@ import com.ocelot.opendevices.api.device.ProcessWindowRenderer;
 import com.ocelot.opendevices.api.laptop.Computer;
 import com.ocelot.opendevices.api.laptop.window.Window;
 import com.ocelot.opendevices.api.util.TooltipRenderer;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.item.ItemStack;
 
 public class TestProcessRenderer implements ProcessWindowRenderer<Computer, TestProcess>
 {
     @Override
     public void render(TestProcess process, Window window, int posX, int posY, int mouseX, int mouseY, float partialTicks)
     {
+        Computer computer = process.getDevice();
     }
 
     @Override
     public void renderOverlay(TooltipRenderer renderer, TestProcess process, Window window, int posX, int posY, int mouseX, int mouseY, float partialTicks)
     {
-        if (window.isWithinContent(mouseX - posX, mouseY - posY, partialTicks))
+        Computer computer = process.getDevice();
+        if (window.getId().equals(computer.getWindowManager().getFocusedWindowId()) && window.isWithinContent(mouseX - posX, mouseY - posY, partialTicks))
         {
             renderer.renderTooltip("dik", mouseX, mouseY);
         }
